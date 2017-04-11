5e32c9ff41c5143b553b3e5615964f58ee7c7ddb
Willian <w.goncalves91@gmail.com>
2017-04-10 23:50:29 -0300

    Improve routes.

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,11 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "questions#index"
   resources :questions
-  resources :great_themes, only: [:index] do
-    resources :knowledge_objects, only: [:index] do
-      resources :abilities, only: [:index]
-    end
+  resources :great_themes, only: :index do
+    resources :knowledge_objects, only: :index
+  end
+
+  resources :knowledge_objects, only: [] do
+    resources :abilities, only: :index
   end
 end
