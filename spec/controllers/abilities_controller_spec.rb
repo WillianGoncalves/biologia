92bc932af52f175d12fb69b3e4852dd94af7f11b
Willian <w.goncalves91@gmail.com>
2017-04-10 23:52:28 -0300

    Improve params in spec.

--- spec/controllers/abilities_controller_spec.rb
+++ spec/controllers/abilities_controller_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe AbilitiesController, type: :controller do
   describe 'GET #index' do
     let(:great_theme) {Fabricate :great_theme}
     let(:knowledge_object) {great_theme.knowledge_objects.first}
-    before {get :index, params: {great_theme_id: great_theme.id, knowledge_object_id: knowledge_object.id}}
+    before {get :index, params: {knowledge_object_id: knowledge_object.id}}
     it {expect(response.status).to eq(200)}
     it {expect(response.content_type).to eq('application/json')}
     it {expect(response.body).to eq(knowledge_object.abilities.to_json)}
