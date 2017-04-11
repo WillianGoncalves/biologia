9ac37d6f147018f4d930394254e98e1b6bc2d33d
Willian <w.goncalves91@gmail.com>
2017-04-10 23:52:58 -0300

    Configure i18n.

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,9 @@ module Biologia
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    config.i18n.available_locales = %w(en pt-BR)
+    config.i18n.default_locale = :"pt-BR"
+    config.i18n.fallbacks = {'pt-BR' => 'en'}
   end
 end
