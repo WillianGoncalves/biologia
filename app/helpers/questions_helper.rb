45b6d002635d4b5401c9c10bca20b6580a64a26b
Willian <w.goncalves91@gmail.com>
2017-04-13 21:36:53 -0300

    Implement new question view.

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,2 +1,10 @@
 module QuestionsHelper
+  def question_type_for_select
+    options = []
+    Question.question_types.keys.each do |value|
+      text = I18n.t("activerecord.attributes.question.enums.question_type.#{value}")
+      options << [text, value]
+    end
+    options
+  end
 end
