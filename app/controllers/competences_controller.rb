cf556d3dcd285d309b2c8f2e4c9024a393af08da
Willian <w.goncalves91@gmail.com>
2017-04-14 10:45:58 -0300

    Add competence controller.

---
+++ app/controllers/competences_controller.rb
@@ -0,0 +1,6 @@
+class CompetencesController < ApplicationController
+  def index
+    @competences = Competence.all
+    render json: @competences
+  end
+end
---
+++ spec/controllers/competences_controller_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe CompetencesController, type: :controller do
+
+end
