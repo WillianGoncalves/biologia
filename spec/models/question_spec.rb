2f30bc1d814ea14cec2ef3f69a13c086f3de5a83
Willian <w.goncalves91@gmail.com>
2017-05-18 14:27:25 -0300

    Implement question model tests.

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Question, type: :model do
   context 'valid questions' do
-    it 'multiple choice' do
+    it 'create a multiple choice question' do
       question = Fabricate.build (:multiple_choice)
       question.answers.first.correct!
       question.save
@@ -11,7 +11,7 @@ RSpec.describe Question, type: :model do
       expect(Question.count).to eq(1)
     end
 
-    it 'discursive' do
+    it 'create a discursive question' do
       question = Fabricate.build (:discursive)
       question.answers.first.correct!
       question.save
@@ -20,7 +20,7 @@ RSpec.describe Question, type: :model do
       expect(Question.count).to eq(1)
     end
 
-    it 'true or false' do
+    it 'create a true or false question' do
       question = Fabricate.build (:true_false)
       question.answers.first.correct!
       question.save
@@ -30,7 +30,7 @@ RSpec.describe Question, type: :model do
     end
   end
 
-  context 'invalid data' do
+  context 'with invalid data' do
     it 'does not create question' do
       question = Question.new(statement: Faker::Lorem.sentence, question_type: :multiple_choice)
       expect(question.multiple_choice?).to eq(true)
@@ -40,6 +40,100 @@ RSpec.describe Question, type: :model do
       expect(question.errors.details[:answers][0][:error]).to be(:wrong_length)
       expect(question.errors.details[:ability][0][:error]).to be(:blank)
       expect(question.errors.details[:competence][0][:error]).to be(:blank)
+    end
+  end
+
+  context 'with invalid number of answers' do
+    context 'with no answers' do
+      describe 'does not create' do
+        it 'multiple choice question' do
+          question = Fabricate.build(:multiple_choice, answers: [])
+          expect(question).to_not be_valid
+          expect(question.errors[:answers]).to_not be_empty
+        end
+
+        it 'discursive question' do
+          question = Fabricate.build(:discursive, answers: [])
+          expect(question).to_not be_valid
+          expect(question.errors[:answers]).to_not be_empty
+        end
+
+        it 'true false question' do
+          question = Fabricate.build(:true_false, answers: [])
+          expect(question).to_not be_valid
+          expect(question.errors[:answers]).to_not be_empty
+        end
+      end
+    end
+
+    context 'with too many answers' do
+      describe 'does not create' do
+        it 'multiple choice question' do
+          question = Fabricate.build(:multiple_choice)
+          aditional_answer = Fabricate.build(:answer)
+          question.answers << aditional_answer
+          expect(question).to_not be_valid
+          expect(question.errors[:answers]).to_not be_empty
+        end
+
+        it 'discursive question' do
+          question = Fabricate.build(:discursive)
+          aditional_answer = Fabricate.build(:answer)
+          question.answers << aditional_answer
+          expect(question).to_not be_valid
+          expect(question.errors[:answers]).to_not be_empty
+        end
+
+        it 'true false question' do
+          question = Fabricate.build(:true_false)
+          aditional_answer = Fabricate.build(:answer)
+          question.answers << aditional_answer
+          expect(question).to_not be_valid
+          expect(question.errors[:answers]).to_not be_empty
+        end
+      end
+    end
+  end
+
+  context 'with invalid number of correct answers' do
+    context 'with to many correct answers' do
+      describe 'does not create' do
+        it 'multiple choice question' do
+          question = Fabricate.build(:multiple_choice)
+          question.answers.each { |a| a.correct! }
+          expect(question).to_not be_valid
+          expect(question.errors[:answers]).to_not be_empty
+        end
+
+        it 'true false question' do
+          question = Fabricate.build(:true_false)
+          question.answers.each { |a| a.correct! }
+          expect(question).to_not be_valid
+          expect(question.errors[:answers]).to_not be_empty
+        end
+      end
+    end
+
+    context 'with no correct answer' do
+      describe 'does not create' do
+        it 'multiple choice question' do
+          question = Fabricate.build(:multiple_choice)
+          expect(question).to_not be_valid
+          expect(question.errors[:answers]).to_not be_empty
+        end
+
+        it 'discursive question' do
+          question = Fabricate.build(:discursive)
+          expect(question).to_not be_valid
+          expect(question.errors[:answers]).to_not be_empty
+        end
+
+        it 'true false question' do
+          question = Fabricate.build(:true_false)
+          expect(question).to_not be_valid
+          expect(question.errors[:answers]).to_not be_empty
+        end
+      end
     end
   end
 end
